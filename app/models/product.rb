561a548450140eeab7b511430ae71c2fd0fb7db8
Newton <anthpaul0@gmail.com>
2015-04-03 13:16:54 -0500

    product validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,5 @@
 class Product < ActiveRecord::Base
+  validates :name, :price, :description, :presence=>true
+  validates :name, :length => { maximum:  30 }
+  validates :description, :length => { maximum:  140 }
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -3,4 +3,10 @@ require 'rails_helper'
 RSpec.describe Product, type: :model do
 
   it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:price) }
+  it { should validate_presence_of(:description) }
+
+  it { should validate_length_of(:name).is_at_most(30) }
+  it { should validate_length_of(:description).is_at_most(140) }
+
 end
