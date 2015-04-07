2e2ca3f7967f542db1894408f4a8402fe3834309
Newton <anthpaul0@gmail.com>
2015-04-07 13:47:55 -0500

    Updated products factory and added stock to product params

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -3,6 +3,7 @@ FactoryGirl.define do
     name { Faker::Commerce.product_name }
     description { Faker::Company.bs }
     price { Faker::Commerce.price }
+    stock { Faker::Number.number(4) }
 
     factory :invalid_product do
       name nil
