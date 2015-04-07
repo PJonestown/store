6cd68707687b9ccc44241415180a5b6a747043fd
Newton <anthpaul0@gmail.com>
2015-04-07 13:44:03 -0500

    Added stock to products. Validations for products

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,11 @@
 class Product < ActiveRecord::Base
-  validates :name, :price, :description, :presence=>true
+  validates :name, :price, :description, :stock, :presence=>true
   validates :name, :length => { maximum:  30 }
   validates :description, :length => { maximum:  140 }
+
+
+  validates :price, :numericality => true
+  validates :stock, :numericality => { only_integer: true }
 
 
 end  
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150403173752) do
+ActiveRecord::Schema.define(version: 20150407183216) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,8 +20,9 @@ ActiveRecord::Schema.define(version: 20150403173752) do
     t.string   "name"
     t.text     "description"
     t.decimal  "price",       precision: 8, scale: 2
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.datetime "created_at",                                      null: false
+    t.datetime "updated_at",                                      null: false
+    t.integer  "stock",                               default: 0
   end
 
 end
